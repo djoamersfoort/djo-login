f6d2232f6bba08a2ee9f452f0c0db3c63d1f5ee7
Jelmer van Arnhem <Jelmerro@users.noreply.github.com>
2022-11-05 14:48:40 +0100

    use uuid + use builtin https instead of axios + add license

--- index.js
+++ index.js
@@ -1,77 +1,84 @@
-const { AuthorizationCode } = require('simple-oauth2')
-const session = require('express-session')
-const axios = require('axios')
+"use strict"
 
-const randomString = length => {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+const {AuthorizationCode} = require("simple-oauth2")
+const session = require("express-session")
+const https = require("https")
+const {randomUUID} = require("crypto")
 
-  return new Array(length).join(chars[Math.floor(Math.random() * chars.length)])
-}
 class login {
-  constructor (id, secret, redirect_uri, scope) {
-    this.redirect_uri = redirect_uri
-    this.scope = scope
-    this.session = session({
-      secret: randomString(15),
-      saveUninitialized: true,
-      cookie: { maxAge: 7 * 24 * 3600 * 1000 },
-      resave: false
-    })
-    this.client = new AuthorizationCode({
-      client: {
-        id,
-        secret
-      },
-      auth: {
-        tokenHost: 'https://leden.djoamersfoort.nl',
-        authorizePath: '/o/authorize/',
-        tokenPath: '/o/token/'
-      }
-    })
-  }
-
-  socketSession = (socket, next) => this.session(socket.request, {}, next)
-  authorize_uri (state = '') {
-    return this.client.authorizeURL({
-      redirect_uri: this.redirect_uri,
-      scope: this.scope,
-      state
-    })
-  }
-
-  requireLogin = (req, res, next) => {
-    this.session(req, res, () => {
-      if (typeof req.session.user === 'undefined') return res.redirect(this.authorize_uri(req.url))
-
-      next()
-    })
-  }
-  callback = (req, res) => {
-    this.session(req, res, async () => {
-      if (typeof req.query.code === 'undefined') return res.send('No')
-
-      const tokenParams = {
-        code: req.query.code,
-        redirect_uri: this.redirect_uri,
-        scope: this.scope
-      }
-      const tokenDetails = await this.client.getToken(tokenParams)
-      const accessToken = tokenDetails.token.access_token
-      axios.get('https://leden.djoamersfoort.nl/api/v1/member/details', {
-        headers: {
-          Authorization: `Bearer ${accessToken}`
-        }
-      }).then(_res => {
-        req.session.djo = _res.data
-        req.session.user = _res.data.id
-
-        return res.redirect(req.query.state)
-      })
-        .catch(() => {
-          return res.send('An error occurred while trying to fetch user data!')
+    constructor(id, secret, redirectUri, scope) {
+        this.redirectUri = redirectUri
+        this.scope = scope
+        this.session = session({
+            "cookie": {"maxAge": 7 * 24 * 3600 * 1000},
+            "resave": false,
+            "saveUninitialized": true,
+            "secret": randomUUID()
         })
+        this.client = new AuthorizationCode({
+            "auth": {
+                "authorizePath": "/o/authorize/",
+                "tokenHost": "https://leden.djoamersfoort.nl",
+                "tokenPath": "/o/token/"
+            },
+            "client": {id, secret}
+        })
+    }
+
+    socketSession = (socket, next) => {
+        this.session(socket.request, {}, next)
+    }
+
+    authorize_uri = (state = "") => this.client.authorizeURL({
+        "redirect_uri": this.redirectUri, "scope": this.scope, state
     })
-  }
+
+    requireLogin = (req, res, next) => {
+        this.session(req, res, () => {
+            if (typeof req.session.user === "undefined") {
+                return res.redirect(this.authorize_uri(req.url))
+            }
+            next()
+        })
+    }
+
+    callback = (req, res) => {
+        this.session(req, res, async() => {
+            if (typeof req.query.code === "undefined") {
+                return res.send("No")
+            }
+            const tokenParams = {
+                "code": req.query.code,
+                "redirect_uri": this.redirectUri,
+                "scope": this.scope
+            }
+            const tokenDetails = await this.client.getToken(tokenParams)
+            const accessToken = tokenDetails.token.access_token
+            const djoReq = https.request(
+                "https://leden.djoamersfoort.nl/api/v1/member/details",
+                {"headers": {"Authorization": `Bearer ${accessToken}`}},
+                djoRes => {
+                    let data = ""
+                    djoRes.on("data", chunk => {
+                        data += chunk.toString()
+                    })
+                    djoRes.on("end", () => {
+                        try {
+                            data = JSON.parse(data)
+                            req.session.djo = data
+                            req.session.user = data.id
+                            return res.redirect(req.query.state)
+                        } catch {
+                            res.send("An error occurred in user data request")
+                        }
+                    })
+                })
+            djoReq.on("error", () => {
+                res.send("An error occurred in user data request")
+            })
+            djoReq.end()
+        })
+    }
 }
 
 module.exports = login
